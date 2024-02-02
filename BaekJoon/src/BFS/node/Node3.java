bcf0c1961f9b8f58d28427cb1abfdd54e567ab63
lcqff <yeonnu82g@gmail.com>
2024-02-02 10:35:02 +0900

    add Node3

---
+++ BaekJoon/src/BFS/node/Node3.java
@@ -0,0 +1,25 @@
+package BFS.node;
+
+public class Node3 {
+    int x;
+    int y;
+    int z;
+
+    public Node3(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+}
